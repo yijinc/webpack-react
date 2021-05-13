2097081435ace1909c32d45b53bff1fd1d13f18e
yijincai <yijincai@souche.com>
2021-05-13 18:34:13 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import img from './header.jpeg';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // 将图像添加到我们已经存在的 div 中。
+    const myIcon = new Image();
+    myIcon.src = img;
+
+    element.appendChild(myIcon);
+
     return element;
 }
 document.body.appendChild(component());
