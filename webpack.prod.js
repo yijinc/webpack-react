47db4b47289dd8594bc460692fa649f2eab7f2e2
yijincai <yijincai@souche.com>
2021-05-17 17:19:01 +0800

    add service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -10,4 +11,12 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     clean: true, // 每次构建前清理 /dist 文件夹
   },
+  plugins: [
+   new WorkboxPlugin.GenerateSW({
+     // 这些选项帮助快速启用 ServiceWorkers
+     // 不允许遗留任何“旧的” ServiceWorkers
+     clientsClaim: true,
+     skipWaiting: true,
+   }),
+  ],
 });
